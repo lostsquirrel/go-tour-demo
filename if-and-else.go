208b9b6ebacc89d0f0d8646273c0ffed02093001
lostsquirrel <songli1988@gmail.com>
2018-06-09 16:02:35 +0800

    add for if

---
+++ if-and-else.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	//fmt.Println(v)
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
+
