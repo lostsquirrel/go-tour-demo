5b980bf9a26c23faff9a6751f8fc264e4a7ec2ee
lostsquirrel <songli1988@gmail.com>
2018-06-15 21:54:06 +0800

    fix exercise

--- exercise-slices.go
+++ exercise-slices.go
@@ -6,7 +6,7 @@ func Pic(dx, dy int) [][]uint8 {
 
 	var res [][]uint8
 	for x := 0; x <= dx; x++ {
-		each := make([]uint8, dx)
+		each := make([]uint8, dx + 1)
 		for y := 0; y <= dy; y++ {
 			//each[y] = uint8((x + y) / 2)
 			//each[y] = uint8(x * y)
