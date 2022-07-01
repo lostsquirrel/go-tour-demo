95a673784435e75b1e228fdcf47a554678367319
lisong <lisong@cdsunrise.net>
2022-07-01 11:42:43 +0800

    add slice tests

--- slices_test.go
+++ slices_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go-tour-demo/utils"
 	"testing"
 )
 
@@ -10,4 +11,46 @@ func TestSlice(t *testing.T) {
 
 	var a []int = primes[1:4]
 	fmt.Println(a)
+}
+
+func TestSliceNew(t *testing.T) {
+	mySlice := new([]int)
+	fmt.Printf("%T\n", mySlice)
+}
+
+func TestSliceMake(t *testing.T) {
+	mySlice := make([]int, 0)
+	fmt.Printf("%T\n", mySlice)
+	utils.PrintSlice(mySlice)
+}
+func TestSliceMake2(t *testing.T) {
+	mySlice := make([]int, 10)
+	fmt.Printf("%T\n", mySlice)
+	utils.PrintSlice(mySlice)
+}
+func TestSliceMake3(t *testing.T) {
+	mySlice := make([]int, 10, 20)
+	fmt.Printf("%T\n", mySlice)
+	utils.PrintSlice(mySlice)
+}
+
+func TestSliceSize(t *testing.T) {
+	a := []int{}
+	b := []int{1, 2, 3}
+	c := a
+	fmt.Printf("%p %p %p\n", a, b, c)
+	d := append(b, 1)
+	fmt.Printf("%p %p %p\n", a, b, d)
+}
+
+func TestSliceValueModify(t *testing.T) {
+	mySlice := []int{1, 2, 3, 4, 5}
+	for _, value := range mySlice {
+		value *= 2
+	}
+	utils.PrintSlice(mySlice)
+	for index := range mySlice {
+		mySlice[index] *= 2
+	}
+	utils.PrintSlice(mySlice)
 }
