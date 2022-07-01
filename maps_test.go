97ec66dd48f7033a1cef40e6b9b4f19b414f7117
lisong <lisong@cdsunrise.net>
2022-07-01 15:33:17 +0800

    add map tests

--- maps_test.go
+++ maps_test.go
@@ -6,10 +6,39 @@ import (
 	"testing"
 )
 
-func TestMap(t *testing.T) {
-	var m map[string]common.FloatVertex
-	m = make(map[string]common.FloatVertex)
-	m["Bell Labs"] = common.FloatVertex{40.68433, -74.39967}
+func TestMapDefine(t *testing.T) {
+	var m0 map[string]common.FloatVertex
+	fmt.Println(m0)
+
+}
+func TestMapDefineAndAccess(t *testing.T) {
+
+	m := make(map[string]common.FloatVertex)
+	m["Bell Labs"] = common.FloatVertex{X: 40.68433, Y: -74.39967}
 	fmt.Println(m)
 	fmt.Println(m["Bell Labs"])
+}
+
+func TestMapFunctioneValue(t *testing.T) {
+	myFuncMap := map[string]func() int{
+		"x": func() int { return 1 },
+	}
+	fmt.Println(myFuncMap)
+	f := myFuncMap["x"]
+	fmt.Println(f())
+}
+
+func TestMapAccess(t *testing.T) {
+	m := map[string]int{"a": 1}
+	v, ok := m["a"]
+	if ok {
+		fmt.Println(v)
+	}
+}
+
+func TestMapIterate(t *testing.T) {
+	m := map[string]int{"a": 1, "b": 2, "c": 3, "d": 4}
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 }
